d6c6f281c0c3c895b09183d5470c9cbaaa9edb0c
Marta McCasland <marta@martamccasland.com>
2014-08-05 21:09:45 -0700

    MAKE IT SO

---
+++ server.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var Hapi = require('hapi');
+var server = new Hapi.Server('localhost', 6420);
+
+server.route({
+  method: 'GET' ,
+  path: '/' ,
+  handler: function (request, reply) {
+    reply('Hello, world!');
+    }
+});
+
+server.route({
+  method: 'GET' ,
+  path: '/{name}' ,
+  handler: function (request, reply) {
+    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+  }
+});
+
+server.start(function () {
+  console.log('Server running at:' , server.info.uri);
+});
